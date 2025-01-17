cc365c83b6a120a628482352d0ab533792c1abea
huanglingbo2 <huanglingbo2@xiaohongshu.com>
2025-01-17 15:46:28 +0800

    gee-web finished first

---
+++ one-month-golang/gee-web/day4-group/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+// 动态路由，即一条路由规则可以匹配某一类型而非某一条固定的路由。例如/hello/:name，可以匹配/hello/geektutu、hello/jack等
+
+func main() {
+	r := gee.New()
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+
+	r.Run(":9999")
+}
